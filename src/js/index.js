a7c08603ae97e8cb289b06b6be29fc28866988af
Sidney Sudatti <s02devmail@protonmail.com>
2023-03-12 21:54:25 -0300

    Refactor: Add card class

--- src/js/index.js
+++ src/js/index.js
@@ -25,228 +25,50 @@ const cards = [
   },
 ];
 
-/*  Transformando os elementos em objetos */
-const editBtn = document.querySelector(".profile__edit-button");
-const generalModal = document.querySelectorAll(".general-modal");
-const modalProfile = document.querySelector(".modal-profile");
-const modalCard = document.querySelector(".modal-card");
-const formProfile = document.querySelector(".form-profile");
-const formCard = document.querySelector(".form-card");
-const btnSubmit = document.querySelector(".modal__submit-btn");
-const nameInput = formProfile.querySelector(".input-field-name");
-const descriptionInput = formProfile.querySelector(".input-field-description");
-const titleInput = document.querySelector(".input-field-title");
-const urlInput = document.querySelector(".input-field-url");
-const profileName = document.querySelector(".profile__name");
-const descriptionProfile = document.querySelector(".profile__about-me");
-const addCardButton = document.querySelector(".profile__add-card-button");
-const btnCloseModal = document
-  .querySelectorAll(".modal__close-btn")
-  .forEach(function (closeButton) {
-    closeButton.addEventListener("click", () => {
-      generalModal.forEach(function (generalModal) {
-        generalModal.classList.remove("overlay");
-      });
-    });
-  });
+const cardContainer = document.querySelector(".pictures-container");
 
-cards.forEach(function (item, index) {
-  createCard(item);
-});
+class Card {
+  constructor(data, cardSelector) {
+    this._title = data.name;
+    this._link = data.link;
+    this._cardSelector = cardSelector;
+  }
 
-/*----  Função para criar o card   ----*/
-function createCard(item) {
-  const card = document.querySelector("#card").content;
-  const cardContainer = document.querySelector(".pictures-container");
-  const cardElement = card.querySelector(".picture-card").cloneNode(true);
-  const deleteBtn = cardElement.querySelector(".picture-card__delete-btn");
-  deleteBtn.addEventListener("click", function () {
-    cards.shift(item);
-    deleteBtn.parentElement.remove();
-  });
-  const likeBtn = cardElement.querySelector(".picture-card__like-btn");
-  likeBtn.addEventListener("click", function () {
-    likeBtn.classList.toggle("picture-card__like-btn_active");
-  });
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".picture-card")
+      .cloneNode(true);
 
-  const cardLink = item.link;
-  const cardName = item.name;
+    return cardElement;
+  }
 
-  cardElement.querySelector(".picture-card__image").src = cardLink;
-  cardElement.querySelector(".picture-card__description").textContent =
-    cardName;
-  cardElement.querySelector(".picture-card__image").alt = cardName + " image";
+  createCard() {
+    this._element = this._getTemplate();
 
-  /*  Evento de click para abrir o popup da imagem */
+    this._element.querySelector(".picture-card__image").src = this._link;
+    this._element.querySelector(
+      ".picture-card__image"
+    ).alt = `${this._title} image`;
+    this._element.querySelector(".picture-card__description").textContent =
+      this._title;
 
-  cardElement
-    .querySelector(".picture-card__image")
-    .addEventListener("click", function () {
-      const popup = document.querySelector("#popup").content;
-      const popupElement = popup
-        .querySelector(".popup-container")
-        .cloneNode(true);
-      popupElement.querySelector(".popup__image").src = cardLink;
-      popupElement.querySelector(".popup__image").alt = cardName + " image";
-      popupElement.querySelector(".popup__title").textContent = cardName;
-      popupElement.classList.add("overlay");
+    return this._element;
+  }
 
-      const btnCloseModal = popupElement.querySelector(".popup__close-btn");
+  _handleOpenPopup() {}
 
-      btnCloseModal.addEventListener("click", closeModal);
-      function closeModal(evt) {
-        popupElement.remove();
-        btnCloseModal.removeEventListener("click", closeModal);
-      }
+  _handleClosePopup() {}
 
-      window.addEventListener("keydown", removePopupWithEscape);
-      function removePopupWithEscape(evt) {
-        if (evt.key === "Escape") {
-          popupElement.remove();
-          window.removeEventListener("keydown", removePopupWithEscape);
-        }
-      }
-
-      popupElement.addEventListener("click", removePopupWithClickOut);
-      function removePopupWithClickOut(evt) {
-        if (evt.target.classList.contains("overlay")) {
-          evt.target.remove(popupElement);
-          popupElement.removeEventListener("click", removePopupWithClickOut);
-        }
-      }
-
-      return cardContainer.append(popupElement);
-    });
-
-  return cardContainer.append(cardElement);
+  _setEventListeners() {}
 }
 
-/*----  Evento para abrir formulário de dados de perfil    ----*/
-
-editBtn.addEventListener("click", () => {
-  nameInput.value = profileName.textContent;
-  descriptionInput.value = descriptionProfile.textContent;
-  modalProfile.classList.add("overlay");
-
-  window.addEventListener("keydown", closeModalWithEscape);
-  function closeModalWithEscape(evt) {
-    if (evt.key === "Escape") {
-      modalProfile.classList.remove("overlay");
-      window.removeEventListener("keydown", closeModalWithEscape);
-    }
-  }
-
-  modalProfile.addEventListener("click", closeModalWithClickOut);
-  function closeModalWithClickOut(evt) {
-    if (evt.target.classList.contains("overlay")) {
-      evt.target.classList.remove("overlay");
-      modalProfile.removeEventListener("click", closeModalWithClickOut);
-    }
-  }
-});
-
-/*----  Enviando dados de perfil via formulário    ----*/
-
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  descriptionProfile.textContent = descriptionInput.value;
-  modalProfile.classList.remove("overlay");
-}
-
-formProfile.addEventListener("submit", handleProfileFormSubmit);
-
-/*----  Evento para abrir formulário para adicionar um card   ----*/
-
-addCardButton.addEventListener("click", () => {
-  modalCard.classList.add("overlay");
-
-  window.addEventListener("keydown", closeModalWithEscape);
-  function closeModalWithEscape(evt) {
-    if (evt.key === "Escape") {
-      modalCard.classList.remove("overlay");
-      window.removeEventListener("keydown", closeModalWithEscape);
-    }
-  }
-
-  modalCard.addEventListener("click", closeModalWithClickOut);
-  function closeModalWithClickOut(evt) {
-    if (evt.target.classList.contains("overlay")) {
-      evt.target.classList.remove("overlay");
-      modalCard.removeEventListener("click", closeModalWithClickOut);
-    }
-  }
-});
-
-/*----  Enviando dados para adicionar card via formulário   ----*/
-
-function handleCardFormSubmit(evt) {
-  evt.preventDefault();
-
-  cards.unshift({ name: titleInput.value, link: urlInput.value });
-
-  const card = document.querySelector("#card").content;
-  const cardContainer = document.querySelector(".pictures-container");
-  const cardElement = card.querySelector(".picture-card").cloneNode(true);
-  const cardLink = urlInput.value;
-  const cardName = titleInput.value;
-  const likeBtn = cardElement.querySelector(".picture-card__like-btn");
-  likeBtn.addEventListener("click", function () {
-    likeBtn.classList.toggle("picture-card__like-btn_active");
+const renderElements = () => {
+  cards.forEach((cardItem) => {
+    const newCard = new Card(cardItem, ".card");
+    const cardElement = newCard.createCard();
+    cardContainer.append(cardElement);
   });
-  const deleteBtn = cardElement.querySelector(".picture-card__delete-btn");
-  deleteBtn.addEventListener("click", function () {
-    cards.shift(card);
-    deleteBtn.parentElement.remove();
-  });
+};
 
-  cardElement.querySelector(".picture-card__image").src = cardLink;
-  cardElement.querySelector(".picture-card__description").textContent =
-    cardName;
-  cardElement.querySelector(".picture-card__image").alt = cardName + " image";
-
-  cardElement
-    .querySelector(".picture-card__image")
-    .addEventListener("click", function () {
-      const popup = document.querySelector("#popup").content;
-      const popupElement = popup
-        .querySelector(".popup-container")
-        .cloneNode(true);
-      popupElement.querySelector(".popup__image").src = cardLink;
-      popupElement.querySelector(".popup__image").alt = cardName + " image";
-      popupElement.querySelector(".popup__title").textContent = cardName;
-
-      const btnCloseModal = popupElement.querySelector(".popup__close-btn");
-      btnCloseModal.addEventListener("click", closeModal);
-      function closeModal(evt) {
-        popupElement.remove();
-        btnCloseModal.removeEventListener("click", closeModal);
-      }
-
-      window.addEventListener("keydown", removePopupWithEscape);
-      function removePopupWithEscape(evt) {
-        if (evt.key === "Escape") {
-          popupElement.remove();
-          window.removeEventListener("keydown", removePopupWithEscape);
-        }
-      }
-
-      popupElement.addEventListener("click", removePopupWithClickOut);
-      function removePopupWithClickOut(evt) {
-        if (evt.target.classList.contains("overlay")) {
-          evt.target.remove(popupElement);
-          popupElement.removeEventListener("click", removePopupWithClickOut);
-        }
-      }
-
-      popupElement.classList.add("overlay");
-
-      return cardContainer.append(popupElement);
-    });
-
-  modalCard.classList.remove("overlay");
-
-  cardContainer.prepend(cardElement);
-}
-
-formCard.addEventListener("submit", handleCardFormSubmit);
+renderElements();
