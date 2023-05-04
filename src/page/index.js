5b8aff0d7bca360af5ddf9af1b6f43a05353978b
Sidney Sudatti <s02devmail@protonmail.com>
2023-05-03 23:46:37 -0300

    refactor: fix webpack.config

--- src/page/index.js
+++ src/page/index.js
@@ -1,4 +1,4 @@
-import "./page/index.css";
+import "./index.css";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import { configObj } from "../utils/constants.js";
